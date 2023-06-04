29425bd205f394d0e48d0c0e5aeba4c134369379
fanxing <fanxing1024.lnly@gmail.com>
2023-06-04 13:30:34 +0800

    层叠动态路由添加 getStaticPaths API

--- app/blog/[slug]/[post]/page.js
+++ app/blog/[slug]/[post]/page.js
@@ -1,3 +1,9 @@
+import { getAllNoteStaticPaths } from '@/lib/tool'
+
 export default function Page({ params }) {
   return <h1>{params.post}</h1>
+}
+
+export async function getStaticPaths() {
+  return getAllNoteStaticPaths('slug', 'post')
 }
--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,9 +1,9 @@
-import { getNoteStaticPaths } from '@/lib/tool'
+import { getNoteListStaticPaths } from '@/lib/tool'
 
 export default function Page({ params }) {
   return <h1>{params.slug}</h1>
 }
 
 export async function getStaticPaths() {
-  return getNoteStaticPaths('slug')
+  return getNoteListStaticPaths('slug')
 }
