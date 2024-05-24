471dc07c4b5fa570a1e2dcc821c590b928542ece
fanxing <fanxing1024.lnly@gmail.com>
2024-05-24 16:28:38 +0800

    feat: 添加blog nav

--- app/blog/[slug]/[post]/page.js
+++ app/blog/[slug]/[post]/page.js
@@ -1,23 +1,42 @@
-import dynamic from 'next/dynamic'
-import { getAllNoteStaticPaths, getNoteContentStaticProps } from '@/lib/tool'
+import dynamic from "next/dynamic";
+import { getAllNoteStaticPaths, getNoteContentStaticProps } from "@/lib/tool";
+import NavLink from "@/components/navLink";
 
-const ServerToClientCode = dynamic(() => import('@/lib/serverToClientCode'), { ssr: false });
+const ServerToClientCode = dynamic(() => import("@/lib/serverToClientCode"), {
+  ssr: false,
+});
 
 export default async function Page({ params }) {
-  const { slug, post } = params
-  const { data, content } = getNoteContentStaticProps(slug, post)
+  const { slug, post } = params;
+  const { data, content } = getNoteContentStaticProps(slug, post);
 
   return (
     <>
-      <h1 className='blog-title' style={{ borderBottom: 0 }}>{data.title}</h1>
-      <div className='markdown-body' dangerouslySetInnerHTML={{ __html: content }} />
+      <h1 className="blog-title" style={{ borderBottom: 0 }}>
+        {data.title}
+      </h1>
+      <div
+        className="markdown-body"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+      <div className="blog-anchor">
+        {data?.nav?.map((anchor, idx) => (
+          <a
+            className="anchor progressive-display"
+            href={`#${anchor.toLowerCase()}`}
+            key={anchor}
+          >
+            <NavLink>{anchor}</NavLink>
+          </a>
+        ))}
+      </div>
 
       {/* 空组件，不引入dom，通过异步引入，实现在服务端组件写入客户端代码 */}
       <ServerToClientCode />
     </>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  return getAllNoteStaticPaths('slug', 'post')
+  return getAllNoteStaticPaths("slug", "post");
 }
