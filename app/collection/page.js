e659c4fdd2f1d5782040bd63d86eb92b664a21c0
fanxing <fanxing1024.lnly@gmail.com>
2024-07-25 14:29:04 +0800

    feat: 添加collection页面内容

--- app/collection/page.js
+++ app/collection/page.js
@@ -1,6 +1,45 @@
+import { collectionConfig } from "@/lib/data-config";
+import Image from "next/image";
+import ClientComponent from "./client";
 
 export default function Page() {
-  return <div>
-    collection
-  </div>
+  return (
+    <div className="grid gap-6 mt-3 grid-cols-n-media">
+      {collectionConfig.map((card, index) => (
+        <a
+          className="collection-card w-56 h-24 overflow-hidden"
+          key={`collection${index}`}
+          href={card.link}
+          target="_blank"
+        >
+          <div className="w-full h-full flex items-center gap-x-4 p-4 cursor-pointer">
+            <div className="icon w-10">
+              <Image
+                src={card.icon}
+                width={40}
+                height={40}
+                alt={card.desc}
+                className="rounded-full max-w-10"
+                style={{ maxWidth: "2.5rem" }}
+              />
+            </div>
+            <div className="content h-full flex flex-col justify-center">
+              <header>{card.title}</header>
+              <p
+                className="overflow-hidden overflow-ellipsis line-clamp-2 text-xs"
+                style={{ color: "var(--blog-card-color)" }}
+              >
+                {card.desc}
+              </p>
+            </div>
+          </div>
+
+          {/* 添加蒙版,样式会出现边框特效 */}
+          {/* <div className="mask"></div> */}
+        </a>
+      ))}
+
+      <ClientComponent />
+    </div>
+  );
 }
--- components/content.js
+++ components/content.js
@@ -1,26 +1,32 @@
 "use client";
-import { useEffect, useContext, useRef } from "react";
+import { useEffect, useContext, useRef, useMemo } from "react";
 import cls from "classnames";
 import { useSelectedLayoutSegments } from "next/navigation";
 import { GlobalContext } from "@/components/globalContext";
 
 const particle_theme_color = {
-  dark: '224,224,224',
-  light: '156,217,249'
-}
+  dark: "224,224,224",
+  light: "156,217,249",
+};
 
 export default function Content({ children }) {
   const { globalState } = useContext(GlobalContext);
 
   const segments = useSelectedLayoutSegments();
+  const isBlogPage = useMemo(() => segments.includes("blog"), [segments]);
   const particleRef = useRef(null);
 
   useEffect(() => {
     if (particleRef.current) {
-      particleRef.current.themeChange(particle_theme_color?.[globalState.theme]);
+      particleRef.current.themeChange(
+        particle_theme_color?.[globalState.theme]
+      );
     } else {
       import("@/lib/bgParticle").then(({ default: BGParticle }) => {
-        particleRef.current = new BGParticle("__next_background", particle_theme_color?.[globalState.theme] || particle_theme_color[1]);
+        particleRef.current = new BGParticle(
+          "__next_background",
+          particle_theme_color?.[globalState.theme] || particle_theme_color[1]
+        );
         particleRef.current.init();
       });
     }
@@ -30,7 +36,7 @@ export default function Content({ children }) {
     <>
       <main
         className={cls("flex flex-col items-center justify-between mt-20", {
-          ["blog-wrapper"]: segments.length,
+          ["blog-wrapper"]: isBlogPage,
         })}
         style={{ minHeight: "calc(100vh - 10rem)" }}
       >
@@ -39,9 +45,7 @@ export default function Content({ children }) {
       <div
         id="__next_background"
         style={
-          segments.length
-            ? { position: "fixed", zIndex: -1 }
-            : { display: "none" }
+          isBlogPage ? { position: "fixed", zIndex: -1 } : { display: "none" }
         }
       />
     </>
